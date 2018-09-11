2ea54e0af7bf37b290ee73c7a22830469d274941
Rocio Isaurralde <rocio.isau@gmail.com>
2018-09-11 17:58:49 -0300

    Agrega metodo calcular perimetro en la clase triangulo

---
+++ Clases/clases/ClaseTriangulo.java
@@ -0,0 +1,9 @@
+package clases;
+
+public class ClaseTriangulo {
+
+	public double calcularPerimetro(double lado1, double lado2, double lado3) {
+		return lado1 + lado2 + lado3;
+	}
+
+}
--- Test/Test/TestTriangulo.java
+++ Test/Test/TestTriangulo.java
@@ -1,24 +1,18 @@
 package Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+
+import clases.ClaseTriangulo;
 
 public class TestTriangulo {
 
 	@Test
 	public void CalcularPerimetroDeUnTriangulo() {
 		ClaseTriangulo triangulo1 = new ClaseTriangulo();
-		Double resultadoEsperado = 6;
+		Double resultadoEsperado = 6.0;
 		Double resultadoObtenido = triangulo1.calcularPerimetro(2.0, 2.0, 2.0);
       assertEquals(resultadoEsperado, resultadoObtenido);
 	}
-	@Test
-	public void CalcularPerimetroDeUnTriangulo() {
-		ClaseTriangulo triangulo1 = new ClaseTriangulo();
-		Double resultadoEsperado = 8;
-		Double resultadoObtenido = triangulo1.calcularPerimetro(8.0, 2.0);
-      assertEquals(resultadoEsperado, resultadoObtenido);
-	}
-
 }
